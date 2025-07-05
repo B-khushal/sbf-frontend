09649b68260d15a8c06d7197a96d50511b586f18
B khushal <khushalprasad242@gmail.com>
2025-07-05 17:49:09 +0530

    cart

--- src/contexts/SettingsContext.tsx
+++ src/contexts/SettingsContext.tsx
@@ -126,7 +126,89 @@ const defaultFooterSettings: FooterSettings = {
   mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3807.3484898316306!2d78.43144207424317!3d17.395055702585967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb971c17e5196b%3A0x78305a92a4153749!2sSpring%20Blossoms%20Florist!5e0!3m2!1sen!2sin!4v1744469050804!5m2!1sen!2sin"
 };
 
-const defaultCategories: Category[] = [];
+const defaultCategories: Category[] = [
+  {
+    id: 'bouquets',
+    name: 'Bouquets',
+    description: 'Handcrafted floral arrangements',
+    image: 'https://images.unsplash.com/photo-1582794543139-8ac9cb0f7b11?ixlib=rb-4.0.3&q=85&w=800&auto=format&fit=crop',
+    link: '/shop/bouquets',
+    enabled: true,
+    order: 0,
+  },
+  {
+    id: 'chocolate',
+    name: 'Chocolate',
+    description: 'Delicious chocolate arrangements',
+    image: 'https://images.unsplash.com/photo-1481391319762-47dff72954d9?ixlib=rb-4.0.3&q=85&w=800&auto=format&fit=crop',
+    link: '/shop/chocolate',
+    enabled: true,
+    order: 1,
+  },
+  {
+    id: 'baskets',
+    name: 'Baskets',
+    description: 'Elegant gift baskets',
+    image: '/images/d3.jpg',
+    link: '/shop/baskets',
+    enabled: true,
+    order: 2,
+  },
+  {
+    id: 'gifts',
+    name: 'Gifts',
+    description: 'Thoughtful presents for any occasion',
+    image: 'https://images.unsplash.com/photo-1549465220-1a8b9238cd48?ixlib=rb-4.0.3&q=85&w=800&auto=format&fit=crop',
+    link: '/shop/gifts',
+    enabled: true,
+    order: 3,
+  },
+  {
+    id: 'plants',
+    name: 'Plants',
+    description: 'Indoor and outdoor greenery',
+    image: 'https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-4.0.3&q=85&w=800&auto=format&fit=crop',
+    link: '/shop/plants',
+    enabled: true,
+    order: 4,
+  },
+  {
+    id: 'birthday',
+    name: 'Birthday',
+    description: 'Perfect floral gifts',
+    image: 'https://images.unsplash.com/photo-1464349095431-e9a21285b5f3?ixlib=rb-4.0.3&q=85&w=800&auto=format&fit=crop',
+    link: '/shop/birthday',
+    enabled: true,
+    order: 5,
+  },
+  {
+    id: 'anniversary',
+    name: 'Anniversary',
+    description: 'Romantic arrangements',
+    image: 'https://images.unsplash.com/photo-1519378058457-4c29a0a2efac?ixlib=rb-4.0.3&q=85&w=800&auto=format&fit=crop',
+    link: '/shop/anniversary',
+    enabled: true,
+    order: 6,
+  },
+  {
+    id: 'sympathy',
+    name: 'Sympathy',
+    description: 'Comforting arrangements',
+    image: 'https://images.unsplash.com/photo-1518709268805-4e9042af2176?ixlib=rb-4.0.3&q=85&w=800&auto=format&fit=crop',
+    link: '/shop/sympathy',
+    enabled: true,
+    order: 7,
+  },
+  {
+    id: 'occasions',
+    name: 'Occasions',
+    description: 'Special celebrations',
+    image: 'https://images.unsplash.com/photo-1464349095431-e9a21285b5f3?ixlib=rb-4.0.3&q=85&w=800&auto=format&fit=crop',
+    link: '/shop/occasions',
+    enabled: true,
+    order: 8,
+  },
+];
 
 const defaultHomeSections: HomeSection[] = [
   { id: 'hero', type: 'hero', title: 'Hero Section', subtitle: '', enabled: true, order: 0 },
