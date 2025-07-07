0bfde924776d809aa17cd994003028c45c26c556
B khushal <khushalprasad242@gmail.com>
2025-07-07 23:30:52 +0530

    payment

--- src/components/ProductDetail.tsx
+++ src/components/ProductDetail.tsx
@@ -528,7 +528,6 @@ const ProductDetail = ({ product, onAddToCart, onReviewSubmit }: ProductDetailPr
                     <Button
                       className="flex-1"
                       onClick={handleAddToCart}
-                      disabled={!customizations}
                     >
                       <ShoppingCart className="mr-2 h-4 w-4" />
                       Add to Cart
